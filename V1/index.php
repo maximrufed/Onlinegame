3a15202931eef2b72b6fd16cfd33769f3eff001c
maximrufed <49642278+maximrufed@users.noreply.github.com>
2020-02-26 19:12:49 +0300

    Add files via upload

---
+++ V1/index.php
@@ -0,0 +1,10 @@
+<head>
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+	<title>Usbwebserver</title>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+	<script type="text/javascript" src="main.js"></script>
+</head>
+<body>
+	
+	
+</body>
